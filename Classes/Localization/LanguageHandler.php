f13a06dcb683aabf0a7a10b20df66efa3b506972
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-08-04 17:04:21 +0200

    Add localization hook

--- Classes/Localization/LanguageHandler.php
+++ Classes/Localization/LanguageHandler.php
@@ -17,10 +17,23 @@ use TYPO3\CMS\Core\Localization\LanguageStore;
 class LanguageHandler extends LanguageStore {
 
 	/**
+	 * @todo implement handler for writing language files
 	 *
+	 * @param string $key       key in the localization file
+	 * @param string $extensionName
+	 * @param string $default   default value of the label
+	 * @param array  $arguments arguments are being passed over to vsprintf
 	 */
-	public function test() {
+	public function handle($key, $extensionName, &$default, $arguments) {
+		#var_dump($key);
+		#var_dump($extensionName);
+		#var_dump($default);
+		#var_dump($arguments);
+		#die('Test');
+		#
+		#		var_dump();
 		// $this->getSupportedExtensions()
+
 	}
 
 }
