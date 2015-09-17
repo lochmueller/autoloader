b72ef42a19240aeb53f94a4e614d828d93a65b05
Tim Lochmüller <Spyker@tims-mbp.speedport_w_724v_typ_a_05011603_00_007>
2015-09-17 22:01:46 +0200

    Prepare release 1.9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@
 $EM_CONF[$_EXTKEY] = array(
     'title'            => 'Autoloader',
     'description'      => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots...',
-    'version'          => '1.8.0',
+    'version'          => '1.9.0',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'Tim Lochmüller',
