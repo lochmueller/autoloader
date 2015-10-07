24b4eb2bb2a11be34390b58a8c63830f3da80d5d
Tim Lochmüller <Spyker@tims-mbp.speedport_w_724v_typ_a_05011603_00_007>
2015-10-07 23:19:12 +0200

    Release 1.9.2

--- Resources/Private/Examples/autoloader_typeconverter/ext_emconf.php
+++ Resources/Private/Examples/autoloader_typeconverter/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => '',
     'constraints' => [
         'depends' => [
-            'autoloader' => '1.9.1-9.9.9',
+            'autoloader' => '1.9.2-9.9.9',
         ],
     ],
 ];
