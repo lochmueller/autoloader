b7f34842f2a49340a76e4ea2802030c7d22f101b
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-11-16 13:33:57 +0100

    Add 7.x renderType

--- Classes/DataSet/Language.php
+++ Classes/DataSet/Language.php
@@ -37,6 +37,7 @@ class Language implements DataSetInterface
                     'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
                     'config' => [
                         'type' => 'select',
+                        'renderType' => 'selectSingle',
                         'foreign_table' => 'sys_language',
                         'foreign_table_where' => 'ORDER BY sys_language.title',
                         'items' => [
@@ -57,6 +58,7 @@ class Language implements DataSetInterface
                     'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.l18n_parent',
                     'config' => [
                         'type' => 'select',
+                        'renderType' => 'selectSingle',
                         'items' => [
                             [
                                 '',
