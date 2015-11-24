54462e3c99f19ddcd9ae6e4a51bf5f37c9ddb7cb
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-11-24 15:49:54 +0100

    Prepare 1.11.1 release

--- Resources/Private/Examples/autoloader_csh/ext_emconf.php
+++ Resources/Private/Examples/autoloader_csh/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => '',
     'constraints' => [
         'depends' => [
-            'autoloader' => '1.11.0-9.9.9',
+            'autoloader' => '1.11.1-9.9.9',
         ],
     ],
 ];
