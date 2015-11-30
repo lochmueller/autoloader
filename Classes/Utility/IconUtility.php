fcfe8404843438deac7e5b46d03a0c05e28b3910
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-11-30 08:14:31 +0100

    Do not use ::class to support PHP 5.4

--- Classes/Utility/IconUtility.php
+++ Classes/Utility/IconUtility.php
@@ -8,8 +8,6 @@
 namespace HDNET\Autoloader\Utility;
 
 use TYPO3\CMS\Backend\Sprite\SpriteManager;
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
-use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 use TYPO3\CMS\Core\Imaging\IconRegistry;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -34,9 +32,9 @@ class IconUtility
         if (GeneralUtility::compat_version('7.0')) {
             $fullIconPath = substr(PathUtility::getAbsoluteWebPath($icon), 1);
             if (StringUtility::endsWith(strtolower($fullIconPath), 'svg')) {
-                $iconProviderClass = SvgIconProvider::class;
+                $iconProviderClass = 'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\SvgIconProvider';
             } else {
-                $iconProviderClass = BitmapIconProvider::class;
+                $iconProviderClass = 'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider';
             }
             /** @var IconRegistry $iconRegistry */
             $iconRegistry = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Imaging\\IconRegistry');
