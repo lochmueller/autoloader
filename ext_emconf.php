f7761168539b435e0beb656b895fdb010a1e045e
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-06-14 15:13:41 +0200

    [TASK] Add travis configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints'      => array(
         'depends' => array(
             'php'   => '5.4.0-0.0.0',
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '6.2.0-8.1.99',
         ),
     ),
 );
