8d605654baa91297b562c8b08b45041435141f2a
Tim Lochmüller <tim@fruit-lab.de>
2016-07-07 11:14:38 +0200

    Bump version to 2.1.1

--- Resources/Private/Examples/autoloader_soap/ext_emconf.php
+++ Resources/Private/Examples/autoloader_soap/ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => '',
     'constraints' => [
         'depends' => [
-            'autoloader' => '1.11.4-9.9.9',
+            'autoloader' => '2.2.0-9.9.9',
         ],
     ],
 ];
