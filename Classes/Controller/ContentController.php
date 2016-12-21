bb8552a644a3f4c7776ae1e5dc00167de18d7638
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-12-21 12:07:28 +0100

    Add options for template override

--- Classes/Controller/ContentController.php
+++ Classes/Controller/ContentController.php
@@ -11,6 +11,7 @@ use HDNET\Autoloader\Utility\ClassNamingUtility;
 use HDNET\Autoloader\Utility\ExtendedUtility;
 use HDNET\Autoloader\Utility\ModelUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
@@ -33,6 +34,20 @@ class ContentController extends ActionController
         $model = ModelUtility::getModel($targetObject, $data);
 
         $view = $this->createStandaloneView();
+
+        $configuration = $this->configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+        $viewConfiguration = $configuration['view'];
+
+        if (isset($viewConfiguration['file'])) {
+            $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName($viewConfiguration['file']));
+        }
+        if (isset($viewConfiguration['layoutRootPaths'])) {
+            $view->setLayoutRootPaths($viewConfiguration['layoutRootPaths']);
+        }
+        if (isset($viewConfiguration['partialRootPaths'])) {
+            $view->setPartialRootPaths($viewConfiguration['partialRootPaths']);
+        }
+
         $view->assignMultiple([
             'data'     => $data,
             'object'   => $model,
