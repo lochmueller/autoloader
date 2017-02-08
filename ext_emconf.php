a6e56f9a6a02b1f4f651d92ede30f305e80c9763
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-02-08 13:36:40 +0100

    Bump version to 2.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@
 $EM_CONF[$_EXTKEY] = array(
     'title'            => 'Autoloader',
     'description'      => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots, TypoScript, TypeConverter, BackendLayouts and take care of createing needed templates, TCA configuration or translations at the right location.',
-    'version'          => '2.1.8',
+    'version'          => '2.2.0',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'Tim Lochmüller',
