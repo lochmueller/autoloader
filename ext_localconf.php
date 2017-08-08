aeadbd15c12a4f97834e2dac5d5723cb711540b4
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-08-08 16:50:55 +0200

    Drop deprecated function

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,10 +16,12 @@ if (!defined('TYPO3_MODE')) {
     'ExtensionId'
 ]);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
-    'autoloader::clearCache',
-    \HDNET\Autoloader\Hooks\ClearCache::class . '->clear'
-);
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['autoloader::clearCache'] = [
+    'callbackMethod' => \HDNET\Autoloader\Hooks\ClearCache::class . '->clear',
+    'csrfTokenCheck' => true
+];
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['writer'] = [
     'xlf' => \HDNET\Autoloader\Localization\Writer\XliffWriter::class,
     'xml' => \HDNET\Autoloader\Localization\Writer\XmlWriter::class,
