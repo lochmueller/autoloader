54dd2a79377418417abee9e26b61a6ddf2991ee9
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-09-22 11:55:41 +0200

    [CLEANUP] Drop double code

---
+++ Classes/Loader/AbstractServerLoader.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Loading AbstractServerLoader.
+ *
+ */
+namespace HDNET\Autoloader\Loader;
+
+use HDNET\Autoloader\Loader;
+use HDNET\Autoloader\LoaderInterface;
+use HDNET\Autoloader\Utility\ClassNamingUtility;
+use HDNET\Autoloader\Utility\FileUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+/**
+ * Loading AbstractServerLoader.
+ */
+abstract class AbstractServerLoader implements LoaderInterface
+{
+    /**
+     * Server name
+     *
+     * @var string
+     */
+    protected $serverName = 'unknown';
+
+    /**
+     * Run the loading process for the ext_tables.php file.
+     *
+     * @param Loader $autoLoader
+     * @param array  $loaderInformation
+     */
+    public function loadExtensionTables(Loader $autoLoader, array $loaderInformation)
+    {
+        foreach ($loaderInformation as $key => $class) {
+            $GLOBALS['TYPO3_CONF_VARS']['AUTOLOADER'][$this->serverName][$key] = $class;
+        }
+
+        return null;
+    }
+
+    /**
+     * Run the loading process for the ext_localconf.php file.
+     *
+     * @param Loader $autoLoader
+     * @param array  $loaderInformation
+     */
+    public function loadExtensionConfiguration(Loader $autoLoader, array $loaderInformation)
+    {
+        foreach ($loaderInformation as $key => $class) {
+            $GLOBALS['TYPO3_CONF_VARS']['AUTOLOADER'][$this->serverName][$key] = $class;
+        }
+
+        return null;
+    }
+}
--- Classes/Loader/SoapServer.php
+++ Classes/Loader/SoapServer.php
@@ -3,10 +3,10 @@
  * Loading SoapServer.
  *
  */
+
 namespace HDNET\Autoloader\Loader;
 
 use HDNET\Autoloader\Loader;
-use HDNET\Autoloader\LoaderInterface;
 use HDNET\Autoloader\Utility\ClassNamingUtility;
 use HDNET\Autoloader\Utility\FileUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
@@ -14,15 +14,22 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 /**
  * Loading SoapServer.
  */
-class SoapServer implements LoaderInterface
+class SoapServer extends AbstractServerLoader
 {
+    /**
+     * Server name
+     *
+     * @var string
+     */
+    protected $serverName = 'Soap';
+
     /**
      * Get all the complex data for the loader.
      * This return value will be cached and stored in the database
      * There is no file monitoring for this cache.
      *
      * @param Loader $autoLoader
-     * @param int    $type
+     * @param int $type
      *
      * @return array
      */
@@ -42,35 +49,5 @@ class SoapServer implements LoaderInterface
         }
 
         return $info;
-    }
-
-    /**
-     * Run the loading process for the ext_tables.php file.
-     *
-     * @param Loader $autoLoader
-     * @param array  $loaderInformation
-     */
-    public function loadExtensionTables(Loader $autoLoader, array $loaderInformation)
-    {
-        foreach ($loaderInformation as $key => $class) {
-            $GLOBALS['TYPO3_CONF_VARS']['AUTOLOADER']['Soap'][$key] = $class;
-        }
-
-        return null;
-    }
-
-    /**
-     * Run the loading process for the ext_localconf.php file.
-     *
-     * @param Loader $autoLoader
-     * @param array  $loaderInformation
-     */
-    public function loadExtensionConfiguration(Loader $autoLoader, array $loaderInformation)
-    {
-        foreach ($loaderInformation as $key => $class) {
-            $GLOBALS['TYPO3_CONF_VARS']['AUTOLOADER']['Soap'][$key] = $class;
-        }
-
-        return null;
     }
 }
