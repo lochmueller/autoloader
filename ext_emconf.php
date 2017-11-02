247bb88bd054ee0ec15aacf73faf977ae07f6246
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-11-02 14:56:50 +0100

    Set version to 4.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'Autoloader',
     'description'      => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots, TypoScript, TypeConverter, BackendLayouts and take care of createing needed templates, TCA configuration or translations at the right location.',
-    'version'          => '4.0.2',
+    'version'          => '4.1.0',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'Tim Lochmüller',
