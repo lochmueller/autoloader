7579070611bf34e9b8a9c274cca88717caa9aaa6
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-05-17 08:34:31 +0200

    Set version to 6.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'Autoloader',
     'description'      => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, FlexForms, Slots, TypoScript, TypeConverter, BackendLayouts and take care of createing needed templates, TCA configuration or translations at the right location.',
-    'version'          => '6.1.0',
+    'version'          => '6.1.1',
     'state'            => 'stable',
     'clearcacheonload' => 1,
     'author'           => 'Tim Lochmüller',
