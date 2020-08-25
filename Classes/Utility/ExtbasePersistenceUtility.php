2ed12010ce68ee016f6a3b0d62f4c6495656b38f
Tim Lochmüller <tim.lochmueller@hdnet.de>
2020-08-25 13:32:26 +0200

    Fix #153 - Add Extbase Persistence generation

---
+++ Classes/Utility/ExtbasePersistenceUtility.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace HDNET\Autoloader\Utility;
+
+use HDNET\Autoloader\SmartObjectRegister;
+
+class ExtbasePersistenceUtility
+{
+    public static function getClassMappingForExtension(string $extension, array $additionalConfiguration = []): array
+    {
+        $objects = self::getSmartObjectsForExtensionKey($extension);
+        $baseConfiguration = self::generateAutoloaderConfiguration($objects);
+
+        return array_merge($baseConfiguration, $additionalConfiguration);
+    }
+
+    protected static function getSmartObjectsForExtensionKey($extensionKey)
+    {
+        $smartObjects = SmartObjectRegister::getRegister();
+        $extensionObjects = [];
+        foreach ($smartObjects as $className) {
+            $objectExtension = ClassNamingUtility::getExtensionKeyByModel($className);
+            if ($objectExtension === $extensionKey) {
+                $extensionObjects[] = $className;
+            }
+        }
+
+        return $extensionObjects;
+    }
+
+    private static function generateAutoloaderConfiguration(array $objects)
+    {
+        $config = [];
+        foreach ($objects as $className) {
+            $table = ModelUtility::getTableNameByModelReflectionAnnotation($className);
+            if ('' !== $table) {
+                $config[$className] = [
+                    'tableName' => $table,
+                ];
+            }
+        }
+
+        return $config;
+    }
+}
