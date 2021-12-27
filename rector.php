ba8a10e18fb20883800dab68f587d9bfe06b81b1
Florian Patruck <florian.patruck@hdnet.de>
2021-12-27 18:55:59 +0100

    feature(static code): Install rector and phpstan and update packages

---
+++ rector.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
+use Ssch\TYPO3Rector\FileProcessor\Composer\Rector\ExtensionComposerRector;
+use Ssch\TYPO3Rector\Rector\General\ExtEmConfRector;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+
+    $containerConfigurator->import(Typo3SetList::TYPO3_76);
+    $containerConfigurator->import(Typo3SetList::TYPO3_87);
+    $containerConfigurator->import(Typo3SetList::TYPO3_95);
+    // $containerConfigurator->import(Typo3SetList::TYPO3_104);
+    // $containerConfigurator->import(Typo3SetList::TYPO3_11);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, Typo3Option::PHPSTAN_FOR_RECTOR_PATH);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
+
+    $parameters->set(Option::PATHS, [
+        'ext_emconf.php',
+        'composer.json',
+        __DIR__ . '/Classes/',
+        __DIR__ . '/Configuration/',
+        __DIR__ . '/Tests/',
+    ]);
+
+    $parameters->set(Option::SKIP, [
+        NameImportingPostRector::class => [
+            'ext_emconf.php',
+            'ext_localconf.php',
+            'ext_tables.php',
+        ],
+    ]);
+
+    $services = $containerConfigurator->services();
+    $services->set(ExtEmConfRector::class);
+    $services->set(ExtensionComposerRector::class);
+};
